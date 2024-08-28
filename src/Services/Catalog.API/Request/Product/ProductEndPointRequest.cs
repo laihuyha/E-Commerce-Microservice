19cd12c4d2c571ad83169ff8c4e3fd99772dfcc1
Huy Hà <laihuyha@gmail.com>
2024-08-28 21:34:48 +0700

    Add new request and response for endpoint

--- src/Services/Catalog.API/Request/Product/ProductEndPointRequest.cs
+++ src/Services/Catalog.API/Request/Product/ProductEndPointRequest.cs
@@ -5,4 +5,5 @@ using Catalog.API.Response.Product;
 namespace Catalog.API.Request.Product
 {
     public record CreateProductRequest(string Name, string Description, List<string> Category, string ImageFile, decimal Price) : ICommand<CreateProductResponse>;
+    public record GetProductsRequest() : IQuery<GetProductsResult>;
 }
--- src/Services/Catalog.API/Response/Product/ProductEndPointResponse.cs
+++ src/Services/Catalog.API/Response/Product/ProductEndPointResponse.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 
 namespace Catalog.API.Response.Product
 {
     public record CreateProductResponse(Guid Id);
+    public record GetProductsResult(IEnumerable<Models.Product> Products);
 }
