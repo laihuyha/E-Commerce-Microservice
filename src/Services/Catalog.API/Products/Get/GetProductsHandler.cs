c2fd7d8e6ff44ada420a8a962ec0a2b5b386375b
Lai Huy Ha <59193067+laihuyha@users.noreply.github.com>
2024-09-14 15:01:02 +0700

    Feat/catalog.api/pagination (#9)
    * Pagination get product
    
    * Add health check and HTTPS config
    
    * Add https profile
    
    * enable https dev certs for image
    
    * Add Health check package and connectionstring to DB

--- src/Services/Catalog.API/Products/Get/GetProductsHandler.cs
+++ src/Services/Catalog.API/Products/Get/GetProductsHandler.cs
@@ -4,16 +4,30 @@ using BuildingBlocks.CQRS;
 using Catalog.API.Models;
 using Catalog.API.Request.Product;
 using Catalog.API.Response.Product;
+using MongoDB.Driver;
 using MongoDB.Entities;
 
 namespace Catalog.API.Products.Get
 {
     public class GetProductsQueryHandler : IQueryHandler<GetProductsRequest, GetProductsResult>
     {
+        private const int DefaultPageSize = 10;
+        private const int DefaultPageNumber = 1;
+        private const int MaxPageSize = 30;
 
         public async Task<GetProductsResult> Handle(GetProductsRequest query, CancellationToken cancellationToken)
         {
-            var products = await DB.Find<Product>().ExecuteAsync(cancellationToken);
+            int pageNumber = query.PageNumber ?? DefaultPageNumber;
+            int pageSize = query.PageSize.HasValue
+                ? (query.PageSize.Value > MaxPageSize ? MaxPageSize : query.PageSize.Value)
+                : DefaultPageSize;
+
+            var (products, totalCount, pageCount) = await DB.PagedSearch<Product>()
+                .Sort(prod => prod.Ascending("Name").Descending("CreatedOn")) // or Sort(prod => prod.Name, Order.Ascending) for simple usage.
+                .PageSize(pageSize)
+                .PageNumber(pageNumber)
+                .ExecuteAsync(cancellationToken);
+
             return new GetProductsResult(products);
         }
     }
