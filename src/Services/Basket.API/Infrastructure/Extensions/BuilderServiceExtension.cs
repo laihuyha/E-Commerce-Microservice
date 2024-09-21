17a5793219c057e5eb9f8699446b303ee2435640
Huy Hà <laihuyha@gmail.com>
2024-09-22 01:29:19 +0700

    Config for Marten, DI, Exceptions

--- src/Services/Basket.API/Infrastructure/Extensions/BuilderServiceExtension.cs
+++ src/Services/Basket.API/Infrastructure/Extensions/BuilderServiceExtension.cs
@@ -1,19 +1,53 @@
+using Basket.API.Application.Interfaces;
+using Basket.API.Infrastructure.Repositories;
+using Basket.API.Infrastructure.Schema.Build;
+using BuildingBlocks.Behaviors;
+using BuildingBlocks.Exceptions.Handler;
 using BuildingBlocks.Utils;
 using Carter;
+using Marten;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Weasel.Core;
 
 namespace Basket.API.Extensions;
 
 public static class BuilderServiceExtension
 {
-    public static IServiceCollection AddBuilderServices(this IServiceCollection services)
+    public static IServiceCollection AddBuilderServices(this IServiceCollection services, IConfiguration configuration, IHostEnvironment host)
     {
-        services.AddCarter();
-        services.AddMediatR(cfg =>
+        _ = services.AddCarter();
+        _ = services.AddMediatR(cfg =>
         {
-            cfg.RegisterServicesFromAssemblies(typeof(Program).Assembly);
-            cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
+            _ = cfg.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+            _ = cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
+            _ = cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
         });
+        _ = services.AddMarten(opt =>
+        {
+            // Establish the connection string to your Marten database
+            opt.Connection(configuration.GetConnectionString("Marten")!);
+
+            // Specify that we want to use STJ as our serializer
+            opt.UseSystemTextJsonForSerialization();
+
+            opt.Schema.BuildSchema();
+
+            // If we're running in development mode, let Marten just take care
+            // of all necessary schema building and patching behind the scenes
+            if (host.IsDevelopment())
+            {
+                opt.AutoCreateSchemaObjects = AutoCreate.All;
+            }
+        }).UseLightweightSessions();
+
+        //Repositories
+        _ = services.AddScoped<IBasketRepository, BasketRepository>();
+
+        // Exception Handler
+        _ = services.AddExceptionHandler<CustomExceptionHandler>();
         return services;
     }
 }
