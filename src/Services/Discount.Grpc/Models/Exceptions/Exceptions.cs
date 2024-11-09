71e747dc40a593a759af029639bcd38c6c56481c
Huy Hà <laihuyha@gmail.com>
2024-11-09 09:10:21 +0700

    Add Exception for handling error

---
+++ src/Services/Discount.Grpc/Models/Exceptions/Exceptions.cs
@@ -0,0 +1,17 @@
+﻿using BuildingBlocks.Exceptions;
+
+namespace Discount.Grpc.Models.Exceptions;
+
+public class NotFoundException : BuildingBlocks.Exceptions.NotFoundException
+{
+    public NotFoundException(string message) : base(message)
+    {
+    }
+}
+
+public class DbOperationException : DbErrorException
+{
+    public DbOperationException(string message) : base(message)
+    {
+    }
+}
