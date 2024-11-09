917bfd0e70f1cd9f3693906c99bc415c3fd0c277
Huy Hà <laihuyha@gmail.com>
2024-11-09 09:12:23 +0700

    Add Rpc extension

---
+++ src/Services/Discount.Grpc/Services/Extensions/RpcExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Discount.Grpc.Models.Exceptions;
+using Grpc.Core;
+
+namespace Discount.Grpc.Services.Extensions;
+
+public static class RpcExtensions
+{
+    public static void ValidateNotNull<T>(this T obj, string message = null) where T : class
+    {
+        if (obj == null)
+        {
+            throw new RpcException(new Status(StatusCode.InvalidArgument,
+                message ?? "Invalid request object"));
+        }
+    }
+
+    public static RpcException ToRpcException(this Exception ex)
+    {
+        var statusCode = ex switch
+        {
+            NotFoundException    => StatusCode.NotFound,
+            DbOperationException => StatusCode.Internal,
+            _                    => StatusCode.Internal
+        };
+
+        return new RpcException(new Status(statusCode, ex.Message));
+    }
+}
